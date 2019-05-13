5c8470c6ed3b9862ade6b34e525e9087dcdf09f1
Pierre-Marie Dartus <p.dartus@salesforce.com>
2019-05-13 16:37:01 +0200

    implement lazy loading

--- src/client/modules/base/cover/cover.js
+++ src/client/modules/base/cover/cover.js
@@ -1,16 +1,47 @@
 import { LightningElement, api } from 'lwc';
 
+const COVER_INTERSECTION_OBSERVER = new IntersectionObserver(
+    entries => {
+        for (const entry of entries) {
+            if (entry.isIntersecting) {
+                entry.target._load();
+            }
+        }
+    },
+    {
+        root: document.body,
+        rootMargin: '100%',
+    },
+);
+
 export default class Cover extends LightningElement {
     @api href;
     @api name;
     @api roundedCorners = false;
 
-    get imgClassName() {
-        return [this.roundedCorners && 'rounded-corners'].filter(Boolean).join(' ');
+    shouldLoad = false;
+
+    @api _load() {
+        // Do nothing if the component was already asked to load
+        if (this.shouldLoad) {
+            return;
+        }
+
+        const img = this.template.querySelector('img');
+
+        // Remove placeholder once the image has loaded
+        img.addEventListener('load', () => {
+            img.classList.add('loaded');
+        });
+
+        img.src = this.href;
     }
 
-    handleImageLoad(event) {
-        const { target } = event;
-        target.classList.add('loaded');
+    connectedCallback() {
+        COVER_INTERSECTION_OBSERVER.observe(this.template.host);
+    }
+
+    get imgClassName() {
+        return [this.roundedCorners && 'rounded-corners'].filter(Boolean).join(' ');
     }
 }
