998b34832f9156c20d17a0747942ceda934b3fa4
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-02-15 08:08:31 +0100

    simplify proxy code

--- src/server/proxy.js
+++ src/server/proxy.js
@@ -1,33 +1,24 @@
 'use strict';
 
+//
 const request = require('request');
 
-const REQUEST_CONFIG = {
-    // Avoid SSL certifications issues.
-    rejectUnauthorized: false,
-};
-
 module.exports = function proxyHandler(req, res, next) {
-    const streamUrl = decodeURIComponent(req.params[0]);
-
-    const requestConfig = {
-        ...REQUEST_CONFIG,
-
-        // Forward headers to enable partial file download for streaming
-        headers: req.headers,
+    const url = decodeURIComponent(req.params[0]);
+    const options = {
+        rejectUnauthorized: false, // Avoid SSL certifications issues.
+        headers: req.headers, // Forward headers to enable partial file download for streaming
     };
 
-    const handleError = err => {
-        const { code } = err;
+    request(url, options)
+        .on('error', err => {
+            const { code } = err;
 
-        if (code === 'ECONNRESET' || code === 'ECONNREFUSED') {
-            return res.sendStatus(503);
-        }
+            if (code === 'ECONNRESET' || code === 'ECONNREFUSED') {
+                return res.sendStatus(503);
+            }
 
-        return next(err);
-    };
-
-    request(streamUrl, requestConfig)
-        .on('error', handleError)
+            return next(err);
+        })
         .pipe(res);
 };
