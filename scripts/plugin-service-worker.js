ecb4e0d1b0d9d2fb448b754d981c2cadaf3ea335
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-02-17 15:21:18 +0100

    make caching working

--- scripts/plugin-service-worker.js
+++ scripts/plugin-service-worker.js
@@ -11,20 +11,27 @@ const { __PROD__ } = require('./shared');
 
 module.exports = () => ({
     name: 'service-worker',
+    buildStart() {
+        this.addWatchFile('src/sw.js');
+    },
     async writeBundle() {
         const manifest = await workbox.getManifest({
             globDirectory: 'dist/',
-            globPatterns: ['**/*.{html,js}'],
+            globPatterns: ['*.{html,js,json}', 'icons/*.png'],
         });
 
         for (const warning of manifest.warnings) {
             this.warn(warning);
         }
 
-        // Correct the gathered manifest URLs.
+        // Correct manifest information:
+        //  - add the `/` to make URL relative to the root
+        //  - remove revision from javascript file because the file name already have their revision encoded in their
+        //    filename.
         manifest.manifestEntries = manifest.manifestEntries.map(entry => ({
             ...entry,
             url: `/${entry.url}`,
+            revision: entry.url.endsWith('.js') ? null : entry.revision,
         }));
 
         const bundle = await rollup({
--- src/sw.js
+++ src/sw.js
@@ -1,17 +1,41 @@
-import { registerRoute } from 'workbox-routing';
-import { CacheFirst } from 'workbox-strategies';
 import { ExpirationPlugin } from 'workbox-expiration';
-import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute, NavigationRoute } from 'workbox-routing';
+import { precacheAndRoute, matchPrecache } from 'workbox-precaching';
+import { StaleWhileRevalidate, NetworkOnly } from 'workbox-strategies';
 
+// Notes:
+//  - No need to cache cover image, the CDN used by spreaker API marks the cover images immutable
+//    and cacheable for 1 year.
+
+const networkOnly = new NetworkOnly();
+
+// Download and cache assets when the server worker is installed. The WORKBOX_MANIFEST environment
+// variable is injected at build time by the service-worker plugin.
 precacheAndRoute(process.env.WORKBOX_MANIFEST);
 
+// Register route for all the browser navigation requests (when Request.mode === 'navigate').
+// Using a network first approach to make sure the HTML file is alway up to date when online, but
+// fallback to the precached index file when offline.
 registerRoute(
-    /.*\.cloudfront\.net\/.*\.(?:png|jpg)$/,
-    new CacheFirst({
-        cacheName: 'covers',
+    new NavigationRoute(async params => {
+        try {
+            return await networkOnly.handle(params);
+        } catch {
+            return matchPrecache('/index.html');
+        }
+    }),
+);
+
+// Register router for all the spreaker.com API requests.
+// Using a stale while revalidate approach to keep the data up to date when online. Cache all the
+// API response for 10 days max.
+registerRoute(
+    /.*api.spreaker.com/,
+    new StaleWhileRevalidate({
+        cacheName: 'spreaker-api',
         plugins: [
             new ExpirationPlugin({
-                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                maxAgeSeconds: 10 * 24 * 60 * 60, // 10 Days
             }),
         ],
     }),
