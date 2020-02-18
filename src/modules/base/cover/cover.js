c5d0d41220cb3f54d87586fdd0ad22c4957e579f
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-02-18 08:15:39 +0100

    Add image placeholder while image loads

--- src/modules/base/cover/cover.js
+++ src/modules/base/cover/cover.js
@@ -6,6 +6,9 @@ let COVER_INTERSECTION_OBSERVER = new IntersectionObserver(
             if (entry.isIntersecting) {
                 const { target } = entry;
 
+                // Invoke the load method on the cover element, and disconnect the intersection
+                // observer to avoid repetitive invocation if the target happen to exit and enter
+                // the viewport again.
                 target._load();
                 COVER_INTERSECTION_OBSERVER.unobserve(target);
             }
@@ -30,13 +33,13 @@ export default class Cover extends LightningElement {
     }
     set href(value) {
         this._href = value;
-        this.handleLoad();
+        this.loadCover();
     }
 
     @api _load() {
         if (!this._inViewport) {
             this._inViewport = true;
-            this.handleLoad();
+            this.loadCover();
         }
     }
 
@@ -48,11 +51,11 @@ export default class Cover extends LightningElement {
         COVER_INTERSECTION_OBSERVER.unobserve(this.template.host);
     }
 
-    get imgClassName() {
-        return [this.roundedCorners && 'rounded-corners'].filter(Boolean).join(' ');
+    get containerClassName() {
+        return `container ${this.roundedCorners ? 'rounded-corners' : ''}`;
     }
 
-    handleLoad() {
+    loadCover() {
         // Wait until the image is in the viewport and the image URL is set before rendering the
         // image.
         if (!this._inViewport && !this._href) {
@@ -64,7 +67,7 @@ export default class Cover extends LightningElement {
             return;
         }
 
-        // Remove placeholder once the image has loaded
+        // Remove placeholder once the image has loaded.
         img.addEventListener('load', () => {
             img.classList.add('loaded');
         });
