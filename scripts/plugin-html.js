aa9fb89977d56c5f61f5acc8dbfbf97e34787aca
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2020-02-22 08:11:37 +0100

    Only preload strict necessary (#13)

--- scripts/plugin-html.js
+++ scripts/plugin-html.js
@@ -2,6 +2,33 @@
 
 const fs = require('fs');
 
+// Returns the application entry point.
+function getEntryChunk(bundle) {
+    return Object.values(bundle).find(entry => entry.type === 'chunk' && entry.isEntry);
+}
+
+// Returns the chunks that need to be preloaded with the rest of the application.
+// The modules to preload are all the modules that are statically imported from the entry module.
+function getModulesToPreload(entryChunk, bundle) {
+    const toProcess = [entryChunk];
+    const toPreload = {};
+
+    while (toProcess.length > 0) {
+        const chunk = toProcess.shift();
+
+        const { imports } = bundle[chunk.fileName];
+
+        // Module might have circular dependencies. If the chunk is already present in the toPreload
+        // object it means that it's dependencies have already been processed.
+        if (!toPreload[chunk.fileName]) {
+            toPreload[chunk.fileName] = chunk;
+            toProcess.push(...imports.map(fileName => bundle[fileName]));
+        }
+    }
+
+    return Object.values(toPreload);
+}
+
 module.exports = () => ({
     name: 'html',
     buildStart() {
@@ -10,12 +37,12 @@ module.exports = () => ({
     generateBundle(options, bundle) {
         let source = fs.readFileSync('src/index.html', 'utf-8');
 
-        const moduleEntry = Object.values(bundle).find(entry => entry.type === 'chunk' && entry.isEntry);
-        const modulesToPreload = Object.values(bundle).filter(entry => entry.type === 'chunk' && !entry.isEntry);
+        const entryChunk = getEntryChunk(bundle);
+        const modulesToPreload = getModulesToPreload(entryChunk, bundle);
 
         source = source.replace(
             '<!-- MODULE_ENTRY -->',
-            `<script type="module" src="/${moduleEntry.fileName}"></script>`,
+            `<script type="module" src="/${entryChunk.fileName}"></script>`,
         );
         source = source.replace(
             '<!-- MODULES_PRELOAD -->',
