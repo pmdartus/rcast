989946dd84c23e38ffe44f4600dc2c283a54ef44
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2020-02-22 06:39:53 +0100

    Implement settings view (#12)

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { execSync } = require('child_process');
+
 const lwc = require('@lwc/rollup-plugin');
 const replace = require('@rollup/plugin-replace');
 const { terser } = require('rollup-plugin-terser');
@@ -11,7 +13,15 @@ const copyAssets = require('./plugin-copy-assets');
 const serviceWorker = require('./plugin-service-worker');
 
 const { __PROD__ } = require('./shared');
-const packageJSON = require('../package.json');
+
+// Heroku doesn't setup git when building the application. The current git commit is set via the
+// SOURCE_VERSION environment variable. https://devcenter.heroku.com/changelog-items/630
+const COMMIT_HASH = (
+    process.env.SOURCE_VERSION ||
+    execSync('git rev-parse HEAD')
+        .toString()
+        .trim()
+).slice(0, 7);
 
 module.exports = {
     input: 'src/main.js',
@@ -40,8 +50,8 @@ module.exports = {
         }),
         replace({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-            'process.env.RELEASE_VERSION': JSON.stringify(packageJSON.version),
-            'process.env.RELEASE_DATE': JSON.stringify(new Date().toLocaleDateString('en-US')),
+            'process.env.COMMIT_HASH': JSON.stringify(COMMIT_HASH),
+            'process.env.RELEASE_DATE': JSON.stringify(Date.now()),
         }),
         serviceWorker(),
         __PROD__ && terser(),
