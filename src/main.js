f4edbb85f4cc61760c24fc0f5b7ed0082396c170
Jose David Rodriguez Velasco <jodarove@gmail.com>
2020-10-12 05:36:23 -0700

    chore: updates lwc and rewrites connectStore wire adapter (#16)

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
-import { createElement, register } from 'lwc';
-import { registerWireService } from 'wire-service';
+import { createElement } from 'lwc';
 
 import 'sw/window';
 import 'base/toastManager';
 import App from 'rcast/app';
 import { store } from 'rcast/store';
 import { connectivityStatusChanged } from 'rcast/store';
-
-// Register custom wire adapters.
-registerWireService(register);
 
 // Dispatch the connectivity status change into Redux.
 function updateOnlineStatus() {
