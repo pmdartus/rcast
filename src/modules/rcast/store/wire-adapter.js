46aad6b9eb00358104495017feb4f18abc407502
Jose David Rodriguez Velasco <jodarove@gmail.com>
2020-11-05 02:19:54 -0800

    refactor(connectStore): dont rely in update and connect calling order (#17)

--- src/modules/rcast/store/wire-adapter.js
+++ src/modules/rcast/store/wire-adapter.js
@@ -2,27 +2,43 @@ export class connectStore {
     dataCallback;
     store;
     subscription;
+    connected = false;
 
     constructor(dataCallback) {
         this.dataCallback = dataCallback;
     }
 
     connect() {
-        const notifyStateChange = () => {
-            const state = this.store.getState();
-            this.dataCallback(state);
-        };
-        this.subscription = this.store.subscribe(notifyStateChange);
-        notifyStateChange();
+        this.connected = true;
+        this.subscribeToStore();
     }
 
     disconnect() {
-        if (this.subscription) {
-            this.subscription();
-        }
+        this.unsubscribeFromStore();
+        this.connected = false;
     }
 
     update(config) {
+        this.unsubscribeFromStore();
         this.store = config.store;
+        this.subscribeToStore();
+    }
+
+    subscribeToStore() {
+        if (this.connected && this.store) {
+            const notifyStateChange = () => {
+                const state = this.store.getState();
+                this.dataCallback(state);
+            };
+            this.subscription = this.store.subscribe(notifyStateChange);
+            notifyStateChange();
+        }
+    }
+
+    unsubscribeFromStore() {
+        if (this.subscription) {
+            this.subscription();
+            this.subscription = undefined;
+        }
     }
 }
